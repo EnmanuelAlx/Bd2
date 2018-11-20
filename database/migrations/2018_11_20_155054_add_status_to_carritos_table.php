34ac3b0e473edbca37f1b237b0368a87ea234132
EnmanuelAlx <EnmanuelAlx@gmail.com>
2018-11-20 11:55:16 -0400

    Añadiendole status a la tabla de carritos

---
+++ database/migrations/2018_11_20_155054_add_status_to_carritos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStatusToCarritosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('carritos', function (Blueprint $table) {
+            $table->integer('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Carritos', function (Blueprint $table) {
+            //
+        });
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Symfony\Component\Translation\PluralizationRules;
 
 class DatabaseSeeder extends Seeder
 {
